ed3480d14ea366458073c02532446569ab897653
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-01 12:14:23 -0300

    aula 56 fix: simple navigation

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,8 +1,6 @@
 import './App.css';
 import React from 'react';
- 
 import { BrowserRouter } from  'react-router-dom' ;
- 
 import Menu from '../components/layout/Menu';
 import Content from '../components/layout/Content';
  
