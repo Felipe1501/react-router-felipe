5a86db4d70898484878ddcec4d571ba5fbd65aaf
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-01 12:29:00 -0300

    fix: editing menu

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -17,10 +17,10 @@ const Menu = props => (
                     <Link to="/param/guedes123">Param #02</Link>
                 </li>
                 <li>
-                    <Link to="/naoExiste">Não Existe</Link>
+                    <Link to="/about">Sobre</Link>
                 </li>
                 <li>
-                    <Link to="/about">Sobre</Link>
+                    <Link to="/naoExiste">Não Existe</Link>
                 </li>
             </ul>
         </nav>
